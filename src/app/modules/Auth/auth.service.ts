5855eb5e8e609618dbbb7bc2bdeb7f7384fc45ae
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-28 21:15:24 +0600

    make sendimageToCloudinary.ts

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -198,7 +198,7 @@ const forgetPassword = async (userId: string) => {
     '10m',
   );
 
-  const resetUILink = `${config.reset_pass_ui_link}?id=${user.id}&token=${resetToken} `;
+  const resetUILink = `${config.reset_pass_ui_link}?id=${user.id}&token=${resetToken} `; //user k link patano lagbe reset 
 
   sendEmail(user.email, resetUILink);
 
