d48cc55f57d582c2c9ece9d8503468de11632755
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-29 21:40:45 +0600

    added the getme route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,6 +4,8 @@ import { createAdminValidationSchema } from '../Admin/admin.validation';
 import { createFacultyValidationSchema } from '../Faculty/faculty.validation';
 import { createStudentValidationSchema } from './../student/student.validation';
 import { UserControllers } from './user.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
@@ -25,4 +27,14 @@ router.post(
   UserControllers.createAdmin,
 );
 
+router.get(
+  '/me',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  UserControllers.getMe,
+);
 export const UserRoutes = router;
