3c36079f109a6edcca4b7e15c734e6423eb1178d
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-29 21:14:46 +0600

    added superAdmin auto

--- src/app/modules/user/user.constant.ts
+++ src/app/modules/user/user.constant.ts
@@ -1,6 +1,8 @@
 export const USER_ROLE = {
-    student: 'student',
-    faculty: 'faculty',
-    admin: 'admin',
-  } as const;
-  
+  superAdmin: 'superAdmin',
+  student: 'student',
+  faculty: 'faculty',
+  admin: 'admin',
+} as const;
+
+export const UserStatus = ['in-progress', 'blocked'];
