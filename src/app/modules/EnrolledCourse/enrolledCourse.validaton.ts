1b1406e189e5f797e8006027068f41d4d92f42a5
shaikot07 <saifulislamshaikat007@gmail.com>
2025-01-05 22:03:12 +0600

    add the zod validation

---
+++ src/app/modules/EnrolledCourse/enrolledCourse.validaton.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+const createEnrolledCourseValidationZodSchema = z.object({
+  body: z.object({
+    offeredCourse: z.string(),
+  }),
+});
+
+
+
+export const EnrolledCourseValidations = {
+  createEnrolledCourseValidationZodSchema,
+//   updateEnrolledCourseMarksValidationZodSchema,
+};
