f53e31f8bd1dcc3f5b4d917d2175aca3b2e70ace
shaikot07 <saifulislamshaikat007@gmail.com>
2025-01-14 22:17:28 +0600

    updated faculty route

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,29 +1,51 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
-import { AcademicFacultyValidation } from './academicFaculty.validation';
+// import { USER_ROLE } from '../User/user.constant';
 import { AcademicFacultyControllers } from './academicFaculty.controller';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-faculty',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     AcademicFacultyValidation.createAcademicFacultyValidationSchema,
   ),
   AcademicFacultyControllers.createAcademicFaculty,
 );
-// get all
-router.get('/', AcademicFacultyControllers.getAllAcademicFaculties);
-// get single
-router.get('/:facultyId', AcademicFacultyControllers.getSingleAcademicFaculty);
 
-// for updated
+router.get(
+  '/:id',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  AcademicFacultyControllers.getSingleAcademicFaculty,
+);
+
 router.patch(
-  '/:facultyId',
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     AcademicFacultyValidation.updateAcademicFacultyValidationSchema,
   ),
   AcademicFacultyControllers.updateAcademicFaculty,
+);
+
+router.get(
+  '/',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  AcademicFacultyControllers.getAllAcademicFaculties,
 );
 
 export const AcademicFacultyRoutes = router;
