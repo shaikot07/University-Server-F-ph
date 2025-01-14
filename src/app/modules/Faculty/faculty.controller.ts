b3351a168b4b6bc3e8e2c68cb9a753737acbcece
shaikot07 <saifulislamshaikat007@gmail.com>
2025-01-14 22:24:45 +0600

    updated the faculty.Controller

--- src/app/modules/Faculty/faculty.controller.ts
+++ src/app/modules/Faculty/faculty.controller.ts
@@ -5,12 +5,12 @@ import { FacultyServices } from './faculty.service';
 
 const getSingleFaculty = catchAsync(async (req, res) => {
   const { id } = req.params;
-  const result = await FacultyServices.getSingleFacultyFromDB( id );
+  const result = await FacultyServices.getSingleFacultyFromDB(id);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Faculty is retrieved successfully',
+    message: 'Faculty is retrieved succesfully',
     data: result,
   });
 });
@@ -21,34 +21,33 @@ const getAllFaculties = catchAsync(async (req, res) => {
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Faculties are retrieved successfully',
-    data: result,
+    message: 'Faculties are retrieved succesfully',
+    meta: result.meta,
+    data: result.result,
   });
 });
 
 const updateFaculty = catchAsync(async (req, res) => {
   const { id } = req.params;
-  // console.log("check data",id);
   const { faculty } = req.body;
-  // console.log("check data",faculty);
-  const result = await FacultyServices.updateFacultyIntoDB( id , faculty);
+  const result = await FacultyServices.updateFacultyIntoDB(id, faculty);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Faculty is updated successfully',
+    message: 'Faculty is updated succesfully',
     data: result,
   });
 });
 
 const deleteFaculty = catchAsync(async (req, res) => {
-  const {  id } = req.params;
+  const { id } = req.params;
   const result = await FacultyServices.deleteFacultyFromDB(id);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Faculty is deleted successfully',
+    message: 'Faculty is deleted succesfully',
     data: result,
   });
 });
