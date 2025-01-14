4e2ee09015edf99f4fdae017028ec74a8d98f635
shaikot07 <saifulislamshaikat007@gmail.com>
2025-01-14 22:14:09 +0600

    updaed faculty services.ts

--- src/app/modules/academicFaculty/academicFaculty.service.ts
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -1,34 +1,50 @@
+import QueryBuilder from '../../builder/QueryBuilder';
+import { AcademicFacultySearchableFields } from './academicFaculty.constant';
 import { TAcademicFaculty } from './academicFaculty.interface';
 import { AcademicFaculty } from './academicFaculty.model';
 
-
-// add faculty  data in to DB 
 const createAcademicFacultyIntoDB = async (payload: TAcademicFaculty) => {
   const result = await AcademicFaculty.create(payload);
   return result;
 };
 
-// get all Faculties data from DB
-const getAllAcademicFacultiesFromDB =async()=>{
-    const result =await AcademicFaculty.find()
-    return result 
-}
-// get single  Faculties by id data from DB
-const getSingleAcademicFacultyFromDB = async(id:string)=>{
-    const result =AcademicFaculty.findById(id)
-    return result
+const getAllAcademicFacultiesFromDB = async (
+  query: Record<string, unknown>,
+) => {
+  const academicFacultyQuery = new QueryBuilder(AcademicFaculty.find(), query)
+    .search(AcademicFacultySearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await academicFacultyQuery.modelQuery;
+  const meta = await academicFacultyQuery.countTotal();
+
+  return {
+    meta,
+    result,
+  };
 };
 
-const updateAcademicFacultyIntoDB=async(id:string, payload:Partial<TAcademicFaculty>)=>{
+const getSingleAcademicFacultyFromDB = async (id: string) => {
+  const result = await AcademicFaculty.findById(id);
+  return result;
+};
 
-    const result = AcademicFaculty.findOneAndUpdate({_id:id},payload,{new:true});
-    return result 
-}
-
+const updateAcademicFacultyIntoDB = async (
+  id: string,
+  payload: Partial<TAcademicFaculty>,
+) => {
+  const result = await AcademicFaculty.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
 
 export const AcademicFacultyServices = {
-    createAcademicFacultyIntoDB,
-    getAllAcademicFacultiesFromDB,
-    getSingleAcademicFacultyFromDB,
-    updateAcademicFacultyIntoDB
+  createAcademicFacultyIntoDB,
+  getAllAcademicFacultiesFromDB,
+  getSingleAcademicFacultyFromDB,
+  updateAcademicFacultyIntoDB,
 };
